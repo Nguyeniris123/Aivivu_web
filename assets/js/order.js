0a83ba395f8973db3f0d3b88eda2b6a12e9fd21b
NamNguyen1909 <140383708+NamNguyen1909@users.noreply.github.com>
2023-08-09 17:20:21 +0700

    order
    box1

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,3 +1,32 @@
+function setupIncrementDecrement() {
+  // Lấy danh sách các nút "+" và "-" trong bảng
+  var buttons = document.querySelectorAll("#lastrow .sub, #lastrow .add");
+
+  // Lặp qua danh sách các nút và thêm sự kiện onclick
+  buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+      // Lấy ô cha chứa nút và input number
+      var parent = this.parentNode;
+      var input = parent.querySelector("input[type='number']");
+
+      // Lấy giá trị hiện tại của input number
+      var value = parseInt(input.value);
+
+      // Lấy giá trị delta tương ứng với nút được ấn
+      var delta = this.classList.contains("add") ? 1 : -1;
+
+      // Tính toán giá trị mới
+      var newValue = value + delta;
+
+      // Giới hạn giá trị mới trong khoảng từ 0 đến 50
+      newValue = Math.max(0, Math.min(50, newValue));
+
+      // Cập nhật giá trị mới cho input number
+      input.value = newValue;
+    });
+  });
+}
+
 window.onload = function() {
     // Chọn tất cả các nút radio
     var radioButtons = document.querySelectorAll('.box1 input[type="radio"]');
@@ -27,4 +56,9 @@ window.onload = function() {
         });
       });
     });
+
+
+    // Gọi function để thiết lập sự kiện trừ/giảm và cộng/tăng giá trị cho các input number
+  setupIncrementDecrement();
+
   };
