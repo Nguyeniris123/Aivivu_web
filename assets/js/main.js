5b90546fd00189b6b1df2dee5f945871b0bf3e52
Hồ Chí Nguyên <136091628+Nguyeniris123@users.noreply.github.com>
2023-08-19 00:06:44 +0700

    Thêm suggestion bảng tìm kiếm trong slider, fix màu sắc index, fix  UI vemaybay.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,6 +96,44 @@ $(document).ready(function() {
     var currentPath = window.location.pathname.split("/").pop().split(".")[0];
     // Thêm class "active" vào thẻ <a> tương ứng với trang hiện tại
     $('.nav a[href="' + currentPath + '.html"]').addClass('active');
+
+    //Gợi ý danh sách slider box tìm kiếm
+    let suggestList = ['Hồ Chí Minh', 'Hà Nội', 'Đà nẵng', 'Huế', 'Nha Trang', 'Đà Lạt', 'Vũng Tàu', 'Sapa',
+    'Phú Quý', 'Quy Nhơn', 'Đài Loan', 'Thái Lan', 'Hoa Kì', 'Đức', 'Úc', 'Malaysia', 'Campuchia', 'Pháp', 'Phan Thiết',
+    'Canada', "Cà Mau", 'Nhật Bản', 'Trung Quốc', "Nga", "Anh", 'TP Vinh'
+    ];
+
+    function suggestions(inputId, suggestId) {
+        $(inputId).on("keyup", function() {
+            let t = $(this).val();
+            let h = "";
+            for (let c of suggestList)
+                if (c.indexOf(t) >= 0)
+                    h+=`<li>${c}</li>`;
+            $(suggestId).html(h);
+        });
+    
+        $(suggestId).on("click", "li", function() {
+            let t = $(this).text();
+            $(inputId).val(t);
+            $(suggestId).html("");
+        });
+    }
+
+    suggestions("#place1", "#suggest1");
+    suggestions("#place2", "#suggest2");
+
+    // Sự kiện click lắng nghe trên cả tài liệu để đóng danh sách gợi ý khi người dùng nhấn ra ngoài
+    $("html").on("click", function() {
+        $("#suggest1").html("");
+        $("#suggest2").html("");
+    });
+    
+    // sự kiện scroll ko thể dùng html, phải dùng window
+    $(window).on("scroll", function() {
+        $("#suggest1").html("");
+        $("#suggest2").html("");
+    });
 });
 
 
